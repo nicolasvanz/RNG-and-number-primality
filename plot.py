32a139e342fea5f496aa4bc1abe0a3092e36fe04
nicolasvanz <nicolas.vanz@outlook.com>
2022-09-22 11:36:00 -0300

    added input file as parameter on plotter

--- plot.py
+++ plot.py
@@ -1,14 +1,16 @@
+import sys
+
 import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
 
-import sys
 
+def main(infilepath, outfilepath):
+    # read data
+    df = pd.read_csv(infilepath)
 
-def main(outfilepath):
-    df = pd.read_csv(outfilepath)
-
+    # get data sets
     bits_list = df["bits"].to_list()
     lcg_list = df["lcg"].to_list()
     lfg_list = df["lfg"].to_list()
@@ -17,17 +19,21 @@ def main(outfilepath):
 
     npa = np.arange(len(bits_list))
 
+    # creating subplots
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
+    # plot lines
     lns1 = ax1.plot(npa, lcg_list, label = "Linear Congruential Generator")
     lns2 = ax1.plot(npa, lfg_list, label = "Lagged Fibonacci Generator")
     lns3 = ax2.plot(npa, difference, label = "LFG / LCG", color = "r")
+
+    # group subplots legends
     lns = lns1 + lns2 + lns3
     labs = [l.get_label() for l in lns]
     plt.legend(lns, labs, loc="upper center")
 
-    # plt.grid()
+    # plot styles
     ax1.grid()
     ax1.set_xlabel("bits amount")
     ax1.set_ylabel("microsseconds")
@@ -41,7 +47,9 @@ def main(outfilepath):
 
     plt.title("LCG vs LFG")
 
-    plt.show()
+    # save
+    plt.savefig(outfilepath, bbox_inches = "tight")
 
 if __name__ == "__main__":
-    main("results.csv")
+    assert(len(sys.argv) == 3)
+    main(sys.argv[1], sys.argv[2])
