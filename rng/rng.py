6f590a6c9f5c43071cae1f9ddba54bc91614b7e4
nicolasvanz <nicolas.vanz@outlook.com>
2022-09-22 11:35:00 -0300

    updating seed after calculation of a random number

--- rng/rng.py
+++ rng/rng.py
@@ -1,9 +1,12 @@
+import random
+
 from rng.randomseed import randint
 
 def seed_update():
-    global seed
+    global seed, seed_digit_range
     seed_digit_range = 256
     seed = randint(10 ** (seed_digit_range - 1), 10 ** seed_digit_range - 1)
+    # seed = random.randrange(10 ** (seed_digit_range - 1), 10 ** seed_digit_range - 1)
 
 def bit_truncate(n, bits):
     length = n.bit_length()
@@ -14,17 +17,20 @@ def bit_truncate(n, bits):
     raise Exception("Expected longer number when truncating bits")
 
 def lagged_fibonacci_generator(bits, j = 128, k = 159):
-    global seed
+    global seed, seed_digit_range
 
     lfg_sequence = [int(digit) for digit in str(seed)]
     
     n = lfg_sequence[-j] + lfg_sequence[-k]
+    first = n
     lfg_sequence.append(n)
 
     while n.bit_length() < bits:
         sequence_next = lfg_sequence[-j] + lfg_sequence[-k]
         n = int(str(n) + str(sequence_next))
         lfg_sequence.append(sequence_next)
+
+    seed = int(str(seed)[1:] + str(first))
 
     return bit_truncate(n, bits)
 
@@ -38,9 +44,17 @@ def linear_congruential_generator(
 
     x = (seed * a + c) % mod
     n = x
+    first = int(str(x)[0])
 
     while (n.bit_length() < bits):
         x = (x * a + c) % mod
         n = int(str(n) + str(x))
 
+    seed = int(str(seed)[1:] + str(first))
+    
     return bit_truncate(n, bits)
+
+if __name__ == "__main__":
+    pass
+else:
+    seed_update()
---
+++ rng_test.py
@@ -0,0 +1,52 @@
+import time
+import sys
+
+from rng.rng import \
+    lagged_fibonacci_generator, \
+    linear_congruential_generator, \
+    seed_update
+
+def main(outfilepath):
+    table = [[
+        "bits",
+        "lcg",
+        "lfg"
+    ]]
+
+    generators = [linear_congruential_generator, lagged_fibonacci_generator]
+    number_bit_lengths = [
+        40,
+        56,
+        80,
+        128,
+        168,
+        224,
+        256,
+        512,
+        1024,
+        2048,
+        4096,
+    ]
+    
+    runs = 1000
+
+    for nbl in number_bit_lengths:
+        table_row = [str(nbl)]
+        for generator in generators:
+            times = []
+            for _ in range(runs):
+                seed_update()
+                start = time.perf_counter_ns() / 1000
+                generator(nbl)
+                end = time.perf_counter_ns() / 1000
+                times.append(end - start)
+            table_row.append(f"{sum(times) / runs}")
+        table.append(table_row)
+
+    with open(outfilepath, "w") as file:
+        file.write("\n".join([",".join(line) for line in table]))
+
+if __name__ == "__main__":
+    assert(len(sys.argv) == 2)
+    main(sys.argv[1])
+
